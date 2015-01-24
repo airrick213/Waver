587d3a484d3dddf7dfd234a2b1607c54974fe99d
Robert Kim <rkty13@gmail.com>
2015-01-24 15:27:10 -0800

    setting up foreman

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
-from flask import Flask
+from config import app, render_template
 import jinja2
-
-app = Flask(__name__)
+import os
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.jade')
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port,debug=True)
