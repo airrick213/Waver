4aebe3caf580907fc76cced0d5ae64888b6d632e
airrick213 <erickim213@gmail.com>
2015-01-24 22:26:33 -0800

    some css stuff

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+//Specify where the static content is
+app.use(express.static("public", __dirname + "/public"));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
